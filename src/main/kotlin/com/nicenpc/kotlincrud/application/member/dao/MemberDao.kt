1657d3a2c8ce8fd23c1656d93f0cab9b8062abb7
Phil Chen <philchen3000tech@gmail.com>
2025-06-07 02:01:49 +0800

    refactor MemberDao and MemberDaoImpl to return MemberDomain and use Optional for findById

--- src/main/kotlin/com/nicenpc/kotlincrud/application/member/dao/MemberDao.kt
+++ src/main/kotlin/com/nicenpc/kotlincrud/application/member/dao/MemberDao.kt
@@ -1,10 +1,11 @@
 package com.nicenpc.kotlincrud.application.member.dao
 
 import com.nicenpc.kotlincrud.domain.member.entity.MemberDomain
+import java.util.Optional
 
 interface MemberDao {
-    fun create(memberDomain: MemberDomain)
-    fun update(memberDomain: MemberDomain)
+    fun create(memberDomain: MemberDomain): MemberDomain
+    fun update(memberDomain: MemberDomain): MemberDomain
     fun delete(id: Long)
-    fun findById(id: Long): MemberDomain?
+    fun findById(id: Long): Optional<MemberDomain>
 }
--- src/main/kotlin/com/nicenpc/kotlincrud/application/member/dao/MemberDaoImpl.kt
+++ src/main/kotlin/com/nicenpc/kotlincrud/application/member/dao/MemberDaoImpl.kt
@@ -2,6 +2,7 @@ package com.nicenpc.kotlincrud.application.member.dao
 
 import com.nicenpc.kotlincrud.domain.member.entity.MemberDomain
 import org.springframework.stereotype.Repository
+import java.util.Optional
 
 @Repository
 class MemberDaoImpl : MemberDao {
@@ -9,22 +10,41 @@ class MemberDaoImpl : MemberDao {
     lateinit var memberRepository: MemberEntityRepository
 
 
-    override fun create(memberDomain: MemberDomain) {
-        val memberEntity = MemberEntity()
-        memberEntity.name = memberDomain.name
-        memberEntity.age = memberDomain.age
-        memberRepository.save(memberEntity)
+    override fun create(memberDomain: MemberDomain): MemberDomain {
+        val entity = convert(memberDomain)
+        val save = memberRepository.save(entity)
+        return convert(save)
     }
 
-    override fun update(memberDomain: MemberDomain) {
-        TODO("Not yet implemented")
+    override fun update(memberDomain: MemberDomain): MemberDomain {
+        val entity = convert(memberDomain)
+        val save = memberRepository.save(entity)
+        return convert(save)
     }
 
     override fun delete(id: Long) {
-        TODO("Not yet implemented")
+        memberRepository.deleteById(id)
     }
 
-    override fun findById(id: Long): MemberDomain? {
-        TODO("Not yet implemented")
+    override fun findById(id: Long): Optional<MemberDomain> {
+        return memberRepository.findById(id).map { entity ->
+            convert(entity)
+        }
+    }
+
+    fun convert(memberDomain: MemberDomain): MemberEntity {
+        return MemberEntity().apply {
+            this.id = memberDomain.id
+            this.name = memberDomain.name
+            this.age = memberDomain.age
+        }
+    }
+
+    fun convert(entity: MemberEntity): MemberDomain {
+        return MemberDomain(
+            id = entity.id,
+            name = entity.name ?: "",
+            age = entity.age ?: 0
+        )
     }
 }
